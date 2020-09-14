f13377b7c46d3473385d52b915eb93f306771225
MMirbach <m.mirbach153@gmail.com>
2020-09-14 16:05:34 +0300

    silly change

--- src/types.cpp
+++ src/types.cpp
@@ -1,6 +1,6 @@
 // License: Apache 2.0. See LICENSE file in root directory.
 // Copyright(c) 2015 Intel Corporation. All Rights Reserved.
-
+//This is matans file. It is changed
 #include "types.h"
 
 #include <algorithm>
